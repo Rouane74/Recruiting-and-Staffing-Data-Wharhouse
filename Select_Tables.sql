b33691efd201ac02e7c3ca95d7f7857b1d1f7780
Rouane74 <61682530+Rouane74@users.noreply.github.com>
2020-03-02 02:35:34 -0600

    Create Select_Tables.sql

---
+++ Select_Tables.sql
@@ -0,0 +1,32 @@
+    USE Bartech
+go
+SELECT * FROM [dbo].[Agri Labor Req IDs]
+SELECT * FROM [dbo].[Agri Labor Reqs]
+SELECT * FROM [dbo].[Dim_MasterDateDim]
+
+SELECT * FROM [dbo].[Ends]
+SELECT * FROM [dbo].[Headcount (Full Mth)]
+
+
+SELECT * FROM [dbo].[Headcount (Full Qtr)]
+SELECT * FROM [dbo].[Headcount (QtrEnd)]
+
+
+SELECT * FROM [dbo].[OLE DB Destination]
+SELECT * FROM [dbo].[Renewals]
+SELECT * FROM [dbo].[Req Activity]
+SELECT * FROM [dbo].[Resumes]
+SELECT * FROM [dbo].[Shortlisting Order]
+SELECT * FROM [dbo].[Spend]worker
+SELECT * FROM [dbo].[Submitted Interviewed Hired]
+SELECT * FROM [dbo].[Submitted Inteviewed Hired WOs]
+SELECT * FROM [dbo].[Timing]
+SELECT * FROM [dbo].[Worker_Details]
+select * from [dbo].[vw_ReqA]
+select * from [dbo].[vw_Spent]
+
+
+
+-----------------------------------------------------------------------------------------
+select * from [dbo].[vw_Ends]
+Select * from [dbo].[vw_Worker]
